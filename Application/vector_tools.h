9d2aecaa61144e12aa3287c2069caba78b6dd86f
Vincent Maire <maire.vincent31@gmail.com>
2017-03-29 17:31:45 +0200

    Operations sur les vecteurs T_3D_coord.

---
+++ Application/vector_tools.h
@@ -0,0 +1,10 @@
+#ifndef VECTOR_TOOLS_H
+#define VECTOR_TOOLS_H
+
+#include "definitions.h"
+
+T_coord_3D add_coord_3D (T_coord_3D p_op1, T_coord_3D p_op2);
+T_coord_3D mean_coord_3D (T_coord_3D p_op1, T_coord_3D p_op2);
+T_coord_3D scalar_time_coord_3D (T_coord_3D p_op, float scalar);
+
+#endif
