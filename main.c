fda479036141260db99e3d410699d7fd8c4f9f1d
Yonggi Choi <toryi@naver.com>
2017-04-26 19:11:39 +0200

    Mise a niveau vers SDK.

--- main.c
+++ main.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "platform.h"
-#include "drivers/mpu9250.h"
-#include "drivers/gpio.h"
+#include "Drivers/mpu9250.h"
+#include "Drivers/led.h"
+#include "Drivers/switch.h"
+#include "Application/definitions.h"
+#include "Application/position.h"
+#include "Application/vector_tools.h"
 
 int main(void)
 {
     /* local declaration */
 
+    T_sensors sensors;
+
+    // Test
+    T_coord_3D data_asp;
+    T_coord_3D data_acc;
+    T_coord_3D data_mag;
+
     /* initialization */
     init_platform();
-    /* Program statement */
+    init_all_mpu(&sensors);
     printf("Hello world!\n");
-    gpio_write(GPIO_LED, 0x0001);
+
+    /* Program statement */
+    int i = 1000;
+
+    while (!0)
+    {
+    	read_all(&data_asp, &data_acc, &data_mag);
+        compute_mpu_infos (&sensors, data_asp, data_acc, data_mag, 0.0005);
+        //printf("ACC : %f \n", data_acc.x);
+        if(test_tap)
+        {
+        	i --;
+        	led_write(LED1);
+        }
+        else
+        {
+        	i = 1000;
+        	led_write(0);
+        }
+    }
+
     cleanup_platform();
     return EXIT_SUCCESS;
 }
